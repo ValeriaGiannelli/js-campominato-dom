9703fc8cbf45f7703bb82b425143e4f92a3590c5
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-14 15:06:08 +0200

    build (bonus): add base project files

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -1,65 +1,11 @@
 // Consegna
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-// BONUS LIVELLO DI DIFFICOLTA'
-
-// // seleziona il livello di difficoltà
-// let selector = document.getElementById("difficulty");
-// console.log(selector);
-
-// let levelDifficulty = selector.value;
-// console.log(levelDifficulty);
-
-// // a seconda del livello la griglia dovrà avere un diverso numero di caselle
-// let numCell;
-
-// // se livello di difficoltà 1 allora le celle sono 100
-// if(levelDifficulty === "diff1"){
-//     numCell = 100;
-//     console.log(numCell);
-// } else if(levelDifficulty === "diff2"){ // se livello di difficoltà 2 le celle sono 81
-//     numCell = 81;
-//     console.log(numCell);
-// } else if(levelDifficulty === "diff3") { // se livello didifficoltà 3 le celle sono 49    
-//     numCell = 49;
-//     console.log(numCell);
-// }
-
-
-
-// CREO LA GRIGLIA DINAMICA
-
-// // prendo l'elemento nella quale dovrà comparire
-// const container = document.querySelector(".grid");
-// console.log(container);
-
-// // creo la griglia interna andando a creare un elemento per ogni spazio che ho (in questo caso 10*10 = 100)
-// for(i = 1; i <= numCell; i++){
-
-//     // creo l'elemento con la funzione
-//     let cell = createElemntWithClass("div", "box")
-
-//     // inserisco che ogni cella abbia il suo numero
-//     cell.append(i);
-
-
-//     // aggiungo un click ad ogni elemento
-//     cell.addEventListener("click",
-//         function(){
-//             // aggiungo una classe alla cella selezionata
-//             cell.classList.add("clicked")
-
-//             // al click stampo in console il "testo" contenuto in ogni cella
-//             console.log(cell.innerText);
-//         }
-//     );
-//     // lo inserisco nel contenitore
-//     container.append(cell);
-
-// }
 
 /****************************************/
 
@@ -71,82 +17,87 @@ const btn = document.querySelector(".start_game");
 
 // al click del bottone aggiungo una classe alla griglia che la farà comparire
 
-
-
-
 btn.addEventListener("click", 
     function(){
-        // BONUS LIVELLO DI DIFFICOLTA'
-        // seleziona il livello di difficoltà
-        let selector = document.getElementById("difficulty");
-        console.log(selector);
 
-        let levelDifficulty = selector.value;
-        console.log(levelDifficulty);
-        
-        // a seconda del livello la griglia dovrà avere un diverso numero di caselle
-        let numCell;
+        /****** CREO L'ARRAY DEI NUMERI CASUALI ********/
 
-        // se livello di difficoltà 1 allora le celle sono 100
-        if(levelDifficulty === "diff1"){
-            numCell = 100;
-            console.log(numCell);
-        } else if(levelDifficulty === "diff2"){ // se livello di difficoltà 2 le celle sono 81
-            numCell = 81;
-            console.log(numCell);
-        } else if(levelDifficulty === "diff3") { // se livello didifficoltà 3 le celle sono 49    
-            numCell = 49;
-            console.log(numCell);
-        }
+        // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
-        // CREO LA GRIGLIA DINAMICA
+        // creo un array vuoto
+        const arrayBombs = [];
+
+        // inserisco in questo array i numeri casuali
+
+        // devo creare 16 numeri casuali, quindi:
+        while (arrayBombs.length < 16){ // continua finché il mio array non è di 16 numeri
+            // creo un numero randomico
+            let randomNum = generateRandomNum(1, 100);
+            // se questo numero non è dentro l'array -> lo inserisco con push
+            if(!arrayBombs.includes(randomNum)){
+                arrayBombs.push(randomNum);
+            }
+
+        } 
+
+        console.log(arrayBombs);  
+
+        // punteggio iniziale della persona
+        let score = 0;
+        console.log("punteggio iniziale:",  score);
+
+        // prendo l'elemento della pagina
+        const userScore = document.getElementById("user_score");
+
+
+        /*******CREO LA GRIGLIA DINAMICA******/
 
         // prendo l'elemento nella quale dovrà comparire
         const container = document.querySelector(".grid");
-        console.log("container iniziale: ", container);
-
-        // cancello le cose nel container
-        container.innerHTML="";
+        // console.log(container);
 
         // creo la griglia interna andando a creare un elemento per ogni spazio che ho (in questo caso 10*10 = 100)
-        for(i = 1; i <= numCell; i++){
+        for(i = 1; i <= 100; i++){
 
             // creo l'elemento con la funzione
             let cell = createElemntWithClass("div", "box")
 
-            // if(numCell === 81){
-            //     cell.classList.add("box_81");
-            // } else if (numCell === 49) {
-            //     cell.classList.add("box_49");
-            // }
-
-            // do una dimensione a seconda della grandezza
-            let radix = Math.sqrt(numCell);
-            
-            let grandezza = 100 / radix;
-            cell.style.width = `${grandezza}%`;
-            cell.style.height = `${grandezza}%`;
-
             // inserisco che ogni cella abbia il suo numero
             cell.append(i);
 
-
-
-            // aggiungo un click ad ogni elemento
+            // aggiungo un click ad ogni elemento se la cella che mi clicca ha lo stesso valore di un numero contenuto nell'arrayBombs allora bomba -> altrimenti continua
             cell.addEventListener("click",
                 function(){
-                    // aggiungo una classe alla cella selezionata
-                    cell.classList.add("clicked")
 
-                    // al click stampo in console il "testo" contenuto in ogni cella
-                    console.log(cell.innerText);
+                    let safe = true; //condizione di partenza altrimenti mi aggiungeva sempre entrambe le classi                  
+
+                    for(j = 0; j < arrayBombs.length; j++){ //cerca dentro all'array se
+                        //se il numero nella cella = ad un numero dell'array -> bomb
+                        if(parseInt(cell.innerText) === arrayBombs[j]){ 
+                            safe = false;
+                            cell.classList.add("bomb");
+                            //quando becca la bomba compare il punteggio
+                            userScore.innerHTML=`Mi dispiace, hai perso! Il tuo punteggio è di: ${score}`;
+                            // console.log("hai perso. Il tuo punteggio è", score);
+                        } 
+                    }
+
+                    if(safe){ // se non ha preso una bomaba continua.
+                        cell.classList.add("safe");
+                        score = score + 1; //score lo dichiaro fuori dal FOR perché altrimenti mi riparte da 0
+                        console.log("punteggio", score);
+                    }
+
+                    if(score === 100 - 16){
+                        userScore.innerHTML=`Congratulazioni, hai vinto! Il tuo punteggio è di: ${score}`;
+                        // console.log("Congratulazioni, hai vinto!", score);
+                    }
                 }
             );
             // lo inserisco nel contenitore
             container.append(cell);
 
         }
-
         container.classList.add("show");
     }
 );
