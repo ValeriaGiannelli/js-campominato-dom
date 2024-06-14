24addf6b6b4604f155c7b8dad08d27553d5acc45
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-14 12:41:55 +0200

    style: add comment for the logic

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 // Consegna
-// L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+
+
 
 
 // CREO LA GRIGLIA DINAMICA
 
 // prendo l'elemento nella quale dovrà comparire
 const container = document.querySelector(".grid");
-console.log(container);
+// console.log(container);
+
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
 // creo la griglia interna andando a creare un elemento per ogni spazio che ho (in questo caso 10*10 = 100)
 for(i = 1; i <= 100; i++){
@@ -26,6 +32,12 @@ for(i = 1; i <= 100; i++){
         function(){
             // aggiungo una classe alla cella selezionata
             cell.classList.add("clicked")
+
+            // In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina.
+
+            // Altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+
+            // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
             // al click stampo in console il "testo" contenuto in ogni cella
             console.log(cell.innerText);
